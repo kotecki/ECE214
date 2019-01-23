0906136685239b9c44ebeef64b789a841149e677
David E. Kotecki <kotecki@maine.edu>
2019-01-23 15:04:36 -0500

    updated Lab1 matlab and hspc files

--- docs/Matlab/ECE214_2019_Lab1_B.m
+++ docs/Matlab/ECE214_2019_Lab1_B.m
@@ -6,22 +6,38 @@
 addpath('/users/Kotecki/CppSim/CppSimShared/HspiceToolbox'); % ngspice matlab toolbox
 
 clear variables; % clear variables
-hspc_filename = 'ECE214_2018_Lab1.hspc'; % set the hspc filename
+hspc_filename = 'ECE214_2019_Lab1.hspc'; % set the hspc filename
 
 %% Simulation control statement
-hspc_addline('.tran 60u 20m', hspc_filename); % define transient simulation and write to hspc file
+hspc_addline('.tran 20u 20m', hspc_filename); % define transient simulation and write to hspc file
 
 %% Generate N logarithmically spaced resistance values
 % range is from 10 Ohms - 10 Meg Ohms
-N = 10;
+N = 30;
 Rvalues = logspace(1,7,N);
+
+%% Single transient plot to ensure the circuit is behaving properly
+hspc_set_param('res', Rvalues(5), hspc_filename); %set resistance value in Lab1.hspc
+ngsim(hspc_filename); % call ngsim and run simulation
+data = loadsig('simrun.raw'); % load simulation reults in variable 'data'
+vout = evalsig(data,'vout'); % store output voltage values as variable 'vout'
+time = evalsig(data,'TIME'); % store time values in variable 'time'
+
+% quick transient plot
+FigHandle = figure('Position', [200, 75, 850, 600]); % figure size and location
+plot(time * 1e3, vout, 'linewidth', 1.5);
+grid on; % turn on the grid
+set(gca, 'fontsize', 16); % increase font size of title and axes text
+xlabel('Time (ms)', 'fontsize', 16); % label x-axis
+ylabel('Output Voltage (V)', 'fontsize', 16); % label y-axis
+title('Output voltage as a function of time'); % add title
 
 %% Loop through all values of R and determine peak voltage
 for i = 1:N
     hspc_set_param('res', Rvalues(i), hspc_filename); %set resistance value in Lab1.hspc
     ngsim(hspc_filename); % call ngsim and run simulation
-    data = loadsig('simrun.raw'); % load simulation reults in variable 'data'
-    vout = evalsig(data,'vout'); % store output voltage values in variable 'vout'
+    data = loadsig('simrun.raw'); % load simulation reults as variable 'data'
+    vout = evalsig(data,'vout'); % store output voltage values as variable 'vout'
     vpeak(i) = abs(min(vout)); % calculate pleak voltage and store in array 'vpeak'
 end
 
@@ -33,6 +49,5 @@ set(gca, 'fontsize', 16); % increase font size of title and axes text
 xlabel('Resistance (\Omega)', 'fontsize', 16); % label x-axis
 ylabel('Output Voltage (V)', 'fontsize', 16); % label y-axis
 title('Expected output voltage as a function of resistance'); % add title
-grid on; % turn on grid
 
 %% End of .m file
